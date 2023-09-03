50c257bb1397b816a33fe91419e77a6325045e13
ke9000 <odakyu9000.8000ke@gmail.com>
2023-09-03 16:04:12 +0900

    test lib-mng-front

--- DJ_nowplay/main.js
+++ DJ_nowplay/main.js
@@ -7,22 +7,48 @@ function click_btn(num) {
 	let hash_tag2_text = hash_tag2.value;
 	let row = table.rows[num];
 	let song_name = row.cells[1].children[0].value;
+	if(song_name){
+		song_name = song_name+"/";
+	}
 	let singer= row.cells[2].children[0].value;
-	let other = row.cells[3].children[0].value;
+	if(singer){
+		singer = singer+"/";
+	}
+	let product = row.cells[3].children[0].value;
+	if(product){
+		product = product;
+	}
+	let other = row.cells[4].children[0].value;
 
 	hash_tag1_text = hash_tag1_text.replace(/#/g, '');
 	hash_tag2_text = hash_tag2_text.replace(/#/g, '');
+	song_name = song_name.replace(/&/g, '＆');
+	singer = singer.replace(/&/g, '＆');
+	product = product.replace(/&/g, '＆');
+	let hashtag_join = hash_tag1_text;
+	if(hash_tag1_text && hash_tag2_text){
+		hashtag_join = hash_tag1_text+","+hash_tag2_text;
+	} else if(hash_tag2_text) {
+		hashtag_join = hash_tag2_text;
+	}
+	other = other.replace(/&/g, '＆');
 	other = other.replace(/\r\n/g, '\n');
 	other = other.replace(/\r/g, '\n');
 	let other_lines = other.split('\n');
 	let other_text = other_lines.join('%0A');
 
-	console.log("No."+num+"\n"+song_name+"\n"+singer+"\n\n"+other_text+"\n"+hash_tag1_text+"\n"+hash_tag2_text);
+	console.log("No."+num+"\n"+song_name+"\n"+singer+"\n"+product+"\n\n"+other_text+"\n"+hash_tag1_text+"\n"+hash_tag2_text);
 
-	let post_text = "No."+num+"%0A"+song_name+"%0A"+singer+"%0A%0A"+other_text+"%0A";
-	let url = "https://twitter.com/share?text="+post_text+"&hashtags="+hash_tag1_text+"%0A&hashtags="+hash_tag2_text+"&url=%0A";
+	//text without tags
+	let join_text = "No."+num+"/"+song_name+singer+product+"%0A"+other_text;
+	console.log(join_text.length);
 
-	window.open(url, 'tweetwindow', 'width=650, height=470, personalbar=0, toolbar=0, scrollbars=1, sizable=1');
+	if(join_text.length <= 144){
+		let url = "https://twitter.com/share?text="+join_text+"&hashtags="+hashtag_join+"&url=%0A";
+		window.open(url, 'tweetwindow', 'width=650, height=470, personalbar=0, toolbar=0, scrollbars=1, sizable=1');
+	} else {
+		alert("投稿可能文字数を超過しています。文字数を減らしてください");
+	}
 }
 
 function addrow(){
@@ -31,14 +57,15 @@ function addrow(){
 	let cell = new Array(5);
 
 	if (table.rows.length >= 1){
-		for(i=0; i<=4; i++){
+		for(i=0; i<=5; i++){
 			cell[i] = row.insertCell(-1);
 		}
 		cell[0].innerHTML = (table.rows.length -1);
 		cell[1].innerHTML = '<input class="form-control" type="text" name="">';
 		cell[2].innerHTML = '<input class="form-control" type="text" name="">';
-		cell[3].innerHTML = '<textarea id="my-textarea" class="form-control" name="" rows="1"></textarea>';
-		cell[4].innerHTML = '<button class="btn btn-primary" type="button" onclick="click_btn('+(table.rows.length -1)+');">投稿</button>';
+		cell[3].innerHTML = '<input class="form-control" type="text" name="">';
+		cell[4].innerHTML = '<textarea id="my-textarea" class="form-control" name="" rows="1"></textarea>';
+		cell[5].innerHTML = '<button class="btn btn-primary" type="button" onclick="click_btn('+(table.rows.length -1)+');">投稿</button>';
 	}
 	
 }
